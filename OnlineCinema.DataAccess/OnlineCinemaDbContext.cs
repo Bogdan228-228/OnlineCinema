5f39815248e8420039b854a0328d3bb2e13ee8dc
Bogdan <boliinyk2008@gmail.com>
2026-07-27 13:43:19 +0300

    Структура API

---
+++ OnlineCinema.DataAccess/OnlineCinemaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineCinema.DataAccess
+{
+    public class OnlineCinemaDbContext : DbContext
+    {
+        public OnlineCinemaDbContext(DbContextOptions<OnlineCinemaDbContext> options) : base(options)
+        {
+        }
+    }
+}
